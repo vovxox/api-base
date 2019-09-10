fc9640fd19f33a48b38439b827f4b05907aff0e1
vovxox1@gmail.com <rypark@nine.com.au>
2019-09-10 21:59:19 +1000

    initial commit

---
+++ flask_minimal/api/__init__.py
@@ -0,0 +1,3 @@
+from .dummy import DummyEndpoint
+from .dummy import HelloEndpoint
+from .dummy import GetIssueEndpoint
--- stop_server.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# read the process ID from the file
-# and send a soft termination signal
-kill -15 `cat app.pid`
-mv app.pid app-pid.old
